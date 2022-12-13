f841c6f8ca48072279f1dfd5cd10348218496ad7
Ochuko Angozi <ochuks33@yahoo.com>
2022-12-13 12:59:36 -0500

    completed section done

---
+++ app.js
@@ -0,0 +1,5 @@
+const credit_card = document.querySelector(".credit_card_form")
+const formbox = credit_card.querySelector("form");
+const cardNameInput = document.querySelector(".cardholder_name");
+
+const cardname = document.querySelector("#data_cardname")
